ccff0bbdfd60ad640e07dcdb6234074faeeafd0b
Annie Yang <annieqiyang@gmail.com>
2021-06-04 14:53:23 -0400

    wrote my_min function

--- AAClasswork/W4D5/execution_time.rb
+++ AAClasswork/W4D5/execution_time.rb
@@ -1,3 +1,15 @@
-def my_min
+def my_min(array)
+    min = array[0]
+    (0...array.length).each do |i1|
+        if array.all?{ |i2| array[i1]<=i2}
+            return array[i1]
+        end 
+    end
+end
 
+list = [ 0, 3, 5, 4, -5, 10, 1, 90 ]
+p my_min(list)  # =>  -5
+
+def largest_contiguous_subsum(array)
+    max = -999999
 end
