4543fb49bd892b3b5ece764f0321a17d8e1911a8
trustinyoon <trustinyoon@gmail.com>
2021-06-07 16:54:30 -0700

    finished p2

--- AAClasswork/W5D1/skeleton/lib/p02_hashing.rb
+++ AAClasswork/W5D1/skeleton/lib/p02_hashing.rb
@@ -4,11 +4,24 @@ end
 
 class Array
   def hash
+    num = 0
+    self.each_with_index do |ele, i|
+      num = num.hash ^ (ele.hash + i.hash)
+    end
+    num
   end
 end
 
 class String
   def hash
+    alpha = ("a".."z").to_a
+    num = 0
+
+    self.each_char.with_index do |ele, i|
+      num = num.hash ^ (alpha.index(ele).hash + i.hash)
+    end
+
+    num
   end
 end
 
@@ -16,6 +29,13 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    num = 0
+
+    temp = self.keys.sort
+    num = num.hash ^ temp.hash
+    num
+
+    # arr = []
+    # self.each do |k, v|
   end
 end
