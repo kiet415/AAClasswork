921047e43c7e935975e3acfcc33a2d60bbf19a75
Jenny Nhan <j3nnynhan@gmail.com>
2021-06-09 11:27:49 -0700

    finished questions

--- AAClasswork/W5D3/import_db.sql
+++ AAClasswork/W5D3/import_db.sql
@@ -1,27 +1,12 @@
--- #Add a users table.
--- Should track fname and lname attributes.
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS questions;
+DROP TABLE IF EXISTS question_follows;
+DROP TABLE IF EXISTS replies;
+DROP TABLE IF EXISTS question_likes;
 
--- Add a questions table.
--- Track the title, the body, and the associated author (a foreign key).
 
--- Add a question_follows table.
--- This should support the many-to-many relationship between questions and users (a user can have many questions she is following, and a question can have many followers).
--- This is an example of a join table; the rows in question_follows are used to join users to questions and vice versa.
+PRAGMA foreign_keys = ON;
 
--- Add a replies table.
--- Each reply should contain a reference to the subject question.
--- Each reply should have a reference to its parent reply.
--- Each reply should have a reference to the user who wrote it.
--- Don't forget to keep track of the body of a reply.
--- "Top level" replies don't have any parent, but all replies have a subject question.
--- It's okay for a column to be self referential; a foreign key can point to a primary key in the same table.
--- Add a question_likes table.
--- Users can like a question.
--- Have references to the user and the question in this table
-
--- Add a question_likes table.
--- Users can like a question.
--- Have references to the user and the question in this table
 CREATE TABLE users (
     id INTEGER PRIMARY KEY,
     fname TEXT NOT NULL,
@@ -32,16 +17,40 @@ CREATE TABLE questions (
     id INTEGER PRIMARY KEY,
     title TEXT NOT NULL,
     body TEXT NOT NULL,
-    users_id INTEGER NOT NULL
+    user_id INTEGER NOT NULL
 
-    FOREIGN KEY (users_id) REFERENCES users(id)
+    FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
 CREATE TABLE question_follows (
     id INTEGER PRIMARY KEY,
-    users_id INTEGER NOT NULL,
-    questions_id INTEGER NOT NULL
+    user_id INTEGER NOT NULL,
+    question_id INTEGER NOT NULL
 
-    FOREIGN KEY (users_id) REFERENCES users(id)
-    FOREIGN KEY (questions_id) REFERENCES questions(id)
+    FOREIGN KEY (user_id) REFERENCES users(id)
+    FOREIGN KEY (question_id) REFERENCES questions(id)
 );
+
+CREATE TABLE replies (
+    id INTEGER PRIMARY KEY,
+    question_id INTEGER NOT NULL,
+    parent_id INTEGER,
+    author_id INTEGER NOT NULL,
+    body TEXT NOT NULL,
+
+    FOREIGN KEY (author_id) REFERENCES users(id),
+    FOREIGN KEY (parent_id) REFERENCES replies(id),
+    FOREIGN KEY (question_id) REFERENCES questions(id)
+
+);
+
+CREATE TABLE question_likes (
+    id INTEGER PRIMARY KEY,
+    user_id INTEGER NOT NULL,
+    question_id INTEGER NOT NULL,
+    user_likes INTEGER NOT NULL
+
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (question_id) REFERENCES questions(id)
+)
+
