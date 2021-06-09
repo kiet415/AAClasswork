d9ad33e5f6062544da6f0e8d10280499cd0cfc64
Jenny Nhan <j3nnynhan@gmail.com>
2021-06-09 14:33:52 -0700

    finished questions table db

--- AAClasswork/W5D3/QuestionsDatabase.rb
+++ AAClasswork/W5D3/QuestionsDatabase.rb
@@ -30,7 +30,7 @@ class User
         SQL
         return nil unless id.length > 0
 
-        Name.new(id.first) # play is stored in an array!
+        Name.new(id.first) 
     end
 
     def self.find_by_name(fname, lname)
@@ -44,14 +44,123 @@ class User
         SQL
         return nil unless name.length > 0
 
-        Name.new(name.first) # play is stored in an array!
+        Name.new(name.first) 
 
+    end
+
+    def create
+        raise "#{self} already in database" if @id
+        QuestionsDatabase.instance.execute(<<-SQL, @fname, @lname)
+        INSERT INTO
+            users (fname, lname)
+        VALUES
+            (?, ?)
+        SQL
+        @id = QuestionsDatabase.instance.last_insert_row_id
+    end
+
+    def update
+        raise "#{self} not in database" unless @id
+        QuestionsDatabase.instance.execute(<<-SQL, @fname, @lname, @id)
+        UPDATE
+            users
+        SET
+            fname = ?, lname = ?
+        WHERE
+            id = ?
+        SQL  
     end
 
 end
 
 class Questions 
-    
+    attr_accessor :title, :body
+
+    def initialize(options)
+        @id = options['id']
+        @title = options['title']
+        @body = options['body']
+        @user_id = options['user_id']
+    end
+
+    def self.find_by_id(id)
+        id = QuestionsDatabase.instance.execute(<<-SQL, id)
+        SELECT
+            *
+        FROM
+            questions
+        WHERE
+            id = ?
+        SQL
+        return nil unless id.length > 0
+
+        Questions.new(id.first) 
+    end
+
+    def self.find_by_title(title)
+        title_hash = QuestionsDatabase.instance.execute(<<-SQL, title)
+        SELECT
+            *
+        FROM
+            questions
+        WHERE
+            title = ?
+        SQL
+        return nil unless title_hash.length > 0
+
+        Questions.new(title_hash.first) 
+    end
+
+    def self.find_by_body(body)
+        body_hash = QuestionsDatabase.instance.execute(<<-SQL, body)
+        SELECT
+            *
+        FROM
+            questions
+        WHERE
+            id = ?
+        SQL
+        return nil unless body_hash.length > 0
+
+        Questions.new(body_hash.first) 
+    end
+
+    def self.find_by_user_id(user_id)
+        user = User.find_by_id(user_id)
+        user_hash = QuestionsDatabase.instance.execute(<<-SQL, user.id)
+        SELECT
+            *
+        FROM
+            questions
+        WHERE
+            id = ?
+        SQL
+
+        user_hash.map { |user| Questions.new(user) }
+    end
+
+    def create
+        raise "#{self} already in database" if @id
+        QuestionsDatabase.instance.execute(<<-SQL, @title, @body, @user_id)
+        INSERT INTO
+            questions (title, body, user_id)
+        VALUES
+            (?, ?, ?)
+        SQL
+        @id = QuestionsDatabase.instance.last_insert_row_id
+    end
+
+    def update
+        raise "#{self} not in database" unless @id
+        QuestionsDatabase.instance.execute(<<-SQL, @title, @body, @user_id, @id)
+        UPDATE
+            questions
+        SET
+            title = ?, body = ?, user_id = ?
+        WHERE
+            id = ?
+        SQL  
+    end
 
 end
 
