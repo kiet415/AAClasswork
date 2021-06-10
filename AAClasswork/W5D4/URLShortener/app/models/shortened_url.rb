fa0a3b72106038309e5a8afdf2553e27b46c4634
Kiet Nguyen <kiet9657@yahoo.com>
2021-06-10 16:57:55 -0700

    Added shortenedUrl migration

---
+++ AAClasswork/W5D4/URLShortener/app/models/shortened_url.rb
@@ -0,0 +1,16 @@
+# == Schema Information
+#
+# Table name: shortened_urls
+#
+#  id         :bigint           not null, primary key
+#  long_url   :text             not null
+#  short_url  :text             not null
+#  user_id    :integer          not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+class ShortenedUrl < ApplicationRecord
+    validates :long_url, :short_url, presence: true
+    validates :long_url, :short_url, uniqueness: true
+    
+end
