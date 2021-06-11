3cfb193548f7b416e32bee918ae7f105b478c3ee
Shirley Tang <shirleytang1996@gmail.com>
2021-06-11 18:40:59 -0400

    finish 01_queries

--- AAClasswork/W5D5/skeleton/movie_buff/01_queries.rb
+++ AAClasswork/W5D5/skeleton/movie_buff/01_queries.rb
@@ -42,6 +42,13 @@ def biggest_cast
   #
   # Find the id and title of the 3 movies with the
   # largest casts (i.e most actors)
+  Movie
+    .select(:id,:title)
+    .joins(:castings)
+    .group('movies.id')
+    .order('COUNT(castings.actor_id) DESC')
+    .limit(3)
+
 
 end
 
@@ -57,7 +64,15 @@ def directed_by_one_of(them)
   # Movie.where(yr: years)
   #
   # Find the id and title of all the movies directed by one of 'them'.
+ 
+  # .where("actors.name IN ?", them)
 
+  Movie
+    .select(:id,:title)
+    .joins(:director)
+    .where(actors: {name: them})
+    
+    
 end
 
 def movie_names_before_1940
@@ -72,4 +87,7 @@ def movie_names_before_1940
   #
   # Use pluck to find the title of all movies made before 1940.
 
+  Movie
+    .where("yr < 1940")
+    .pluck(:title)
 end
