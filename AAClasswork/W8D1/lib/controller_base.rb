15db5c6ac62d9526428aa20cd36b97233a1de733
Kiet Nguyen <kiet9657@yahoo.com>
2021-06-28 17:31:20 -0700

    Phase 3

--- AAClasswork/W8D1/lib/controller_base.rb
+++ AAClasswork/W8D1/lib/controller_base.rb
@@ -2,32 +2,56 @@ require 'active_support'
 require 'active_support/core_ext'
 require 'erb'
 require_relative './session'
+require 'byebug'
 
 class ControllerBase
   attr_reader :req, :res, :params
 
   # Setup the controller
   def initialize(req, res)
+    @req = req
+    @res = res
   end
 
   # Helper method to alias @already_built_response
   def already_built_response?
+    @already_built_response 
   end
 
   # Set the response status code and header
   def redirect_to(url)
+    if !already_built_response?
+      @res.status = 302
+      @res['Location'] = url
+      @already_built_response = true
+    else
+      raise "Double render error"
+    end
   end
 
   # Populate the response with content.
   # Set the response's content type to the given type.
   # Raise an error if the developer tries to double render.
   def render_content(content, content_type)
+    #debugger
+    if !already_built_response?
+      @res.write(content)
+      @res["Content-Type"] = content_type
+      @already_built_response = true
+    else
+      raise "Double render error"
+    end
+     
   end
 
   # use ERB and binding to evaluate templates
   # pass the rendered html to render_content
   def render(template_name)
+    file = File.open(template_name)
+    template = ERB.new('<%= file.read %>') 
+    
   end
+
 
   # method exposing a `Session` object
   def session
