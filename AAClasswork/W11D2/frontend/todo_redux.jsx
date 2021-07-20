c212e36183e916ddb265db31f94dfa25797def5d
Kiet Nguyen <kiet9657@yahoo.com>
2021-07-20 12:16:45 -0700

    W11D2 setup CW

---
+++ AAClasswork/W11D2/frontend/todo_redux.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+document.addEventListener("DOMContentLoaded", (event) => {
+    
+    ReactDOM.render(<h1>Todos App</h1>,document.querySelector("#root"));
+})
