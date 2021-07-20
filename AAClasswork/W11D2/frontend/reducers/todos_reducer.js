23ff6ace4f410e44dd044a3258b53cfe42f4269e
Kiet Nguyen <kiet9657@yahoo.com>
2021-07-20 15:17:13 -0700

    Todos reducer done

--- AAClasswork/W11D2/frontend/reducers/todos_reducer.js
+++ AAClasswork/W11D2/frontend/reducers/todos_reducer.js
@@ -1,13 +1,37 @@
 import { RECEIVE_TODOS } from "../actions/todo_actions";
+import { RECEIVE_TODO } from "../actions/todo_actions";
+const initialState = {
+    1: {
+      id: 1,
+      title: "wash car",
+      body: "with soap",
+      done: false
+    },
+    2: {
+      id: 2,
+      title: "wash dog",
+      body: "with shampoo",
+      done: true
+    }
+  };
 
-export const todosReducer = (oldState = {}, action) => {
+export const todosReducer = (oldState = initialState, action) => {
     Object.freeze(oldState);
     const nextState = Object.assign({}, oldState)
-
+    
     switch (action.type) {
         case RECEIVE_TODOS:
-            nextState[action.todos.id] = action.todos
+            // action.forEach(todo  => {
+            //     nextState[todo.id] = todo;
+            // } )
+            for(let i = 0; i < action.length; i++) {
+                nextState[action[i].id] = action[i];
+            }
+            //nextState[action.todos.id] = action.todos
             return nextState;
+        case RECEIVE_TODO:
+            const newTODO = {[action.todo.id] : action.todo}
+            return Object.assign({}, oldState , newTODO);
         default:
             return oldState;
     }
--- AAClasswork/W11D2/frontend/todo_redux.jsx
+++ AAClasswork/W11D2/frontend/todo_redux.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import {todosReducer} from "./actions/todo_actions"
-
+import rootReducer from "../frontend/reducers/root_reducer";
+import configureStore from "../frontend/store/store";
+import {receiveTodos, receiveTodo} from "../frontend/actions/todo_actions"
 
 
 document.addEventListener("DOMContentLoaded", (event) => {
-    
     ReactDOM.render(<h1>Todos App</h1>,document.querySelector("#root"));
+    const store = configureStore();
+    window.store = store;
+    window.receiveTodos = receiveTodos;
+    window.receiveTodo = receiveTodo;
 })
