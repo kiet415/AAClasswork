b50f80cf7df907bbe07dba97a0d63fba8f586bfd
Kiet Nguyen <kiet9657@yahoo.com>
2021-07-23 17:03:02 -0700

    entry file created

--- AAClasswork/W11D5/skeleton/frontend/pokedex.jsx
+++ AAClasswork/W11D5/skeleton/frontend/pokedex.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import {fetchAllPokemon} from "./util/api_util"
+import configureStore from './store/store';
 document.addEventListener("DOMContentLoaded", () => {
     const rootEl = document.getElementById('root');
     ReactDOM.render(<h1>Pokedex</h1>, rootEl);
+    const store = configureStore();
+  
+  // More testing
+    window.store = store;
+    window.fetchAllPokemon = fetchAllPokemon;
 })
---
+++ AAClasswork/W11D5/skeleton/frontend/reducers/pokemon_reducer.js
@@ -0,0 +1,7 @@
+const pokemonReducer = (state = {}, action) => {
+    Object.freeze(state);
+    const nextState = Object.assign({}, state);
+    switch(action.type) {
+        
+    }
+}
