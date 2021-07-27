af71235869d9ac6cdebfefa30d02f3bdcdf3e6ac
Kiet Nguyen <kiet9657@yahoo.com>
2021-07-27 14:43:36 -0700

    W12D2 Part 1

---
+++ AAClasswork/W12D2/BenchBnB/app/controllers/users_controller.rb
@@ -0,0 +1,29 @@
+class UsersController < ApplicationController
+
+    def new
+        @user = User.new
+        render :new
+    end
+
+    def create 
+        @user = User.new(user_params)
+        if @user.save
+            login!(@user)
+            redirect_to user_url(@user)
+        else
+            render json: @user.errors.full_messages, status: 422
+        end
+    end
+
+    # def index
+
+    # end
+
+    # def show
+
+    # end
+
+    def user_params
+        params.require(:user).permit(:username, :password)
+    end
+end
