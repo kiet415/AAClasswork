da18a6f61b9ebbb03f0fcd4c6637f5155309a7e9
Kiet Nguyen <kiet9657@yahoo.com>
2021-07-27 13:57:44 -0700

    W12D2 setup complete

---
+++ AAClasswork/W12D2/BenchBnB/app/assets/javascripts/bundle.js
@@ -0,0 +1,25 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./frontend/bench_bnb.jsx":
+/*!********************************!*\
+  !*** ./frontend/bench_bnb.jsx ***!
+  \********************************/
+/***/ (() => {
+
+throw new Error("Module build failed (from ./node_modules/babel-loader/lib/index.js):\nError: Cannot find module 'babel-preset-env'\n- Did you mean \"@babel/env\"?\n    at Function.Module._resolveFilename (internal/modules/cjs/loader.js:580:15)\n    at resolve (/mnt/c/Users/K/Desktop/CS Stuff/App Academy/AAClasswork/W12D2/BenchBnB/node_modules/v8-compile-cache/v8-compile-cache.js:164:23)\n    at resolveStandardizedName (/mnt/c/Users/K/Desktop/CS Stuff/App Academy/AAClasswork/W12D2/BenchBnB/node_modules/@babel/core/lib/config/files/plugins.js:111:7)\n    at resolvePreset (/mnt/c/Users/K/Desktop/CS Stuff/App Academy/AAClasswork/W12D2/BenchBnB/node_modules/@babel/core/lib/config/files/plugins.js:59:10)\n    at loadPreset (/mnt/c/Users/K/Desktop/CS Stuff/App Academy/AAClasswork/W12D2/BenchBnB/node_modules/@babel/core/lib/config/files/plugins.js:78:20)\n    at loadPreset.next (<anonymous>)\n    at createDescriptor (/mnt/c/Users/K/Desktop/CS Stuff/App Academy/AAClasswork/W12D2/BenchBnB/node_modules/@babel/core/lib/config/config-descriptors.js:187:16)\n    at createDescriptor.next (<anonymous>)\n    at step (/mnt/c/Users/K/Desktop/CS Stuff/App Academy/AAClasswork/W12D2/BenchBnB/node_modules/gensync/index.js:261:32)\n    at evaluateAsync (/mnt/c/Users/K/Desktop/CS Stuff/App Academy/AAClasswork/W12D2/BenchBnB/node_modules/gensync/index.js:291:5)");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module doesn't tell about it's top-level declarations so it can't be inlined
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./frontend/bench_bnb.jsx"]();
+/******/ 	
+/******/ })()
+;
+//# sourceMappingURL=bundle.js.map
